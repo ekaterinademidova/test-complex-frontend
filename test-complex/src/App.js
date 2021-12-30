17aef93af594623c7e5c0e3da9937a8e4fec7118
Ekaterina Demidova <86684392+ekaterinademidova@users.noreply.github.com>
2021-12-30 19:03:37 +0300

    feat: basic layout (#1)
    * feat: add basic layout
    
    * fix: edit styles for nav and footer
    
    * feat: add initial layout of the login
    
    * feat: add request
    
    * feat: edit logic and add dynamics to the button

---
+++ test-complex/src/App.js
@@ -0,0 +1,51 @@
+import React from 'react';
+import './App.css';
+import styled from 'styled-components';
+import {Route, BrowserRouter as Router, NavLink, Routes} from 'react-router-dom';
+import Home from './Pages/Home';
+import Dashboard from './Pages/Dashboard';
+import Login from './Pages/Login';
+
+const Nav = styled.nav`
+  height: 70px;
+  width: 100%;
+  background-color: #8DD2E8; 
+  line-height: 70px; 
+  display: flex;
+  justify-content: flex-end;
+`;
+
+const Footer = styled.footer`
+  box-sizing: border-box;
+  height: 70px;
+  width: 100%;
+  padding: 0 30px;
+  background-color: #8DD2E8; 
+  line-height: 70px; 
+  display: flex;
+  justify-content: flex-start;
+`;
+
+function App() {
+  return (
+    <Router>
+      <div className='container'>
+        <Nav>
+          <NavLink to='/' className='nav-link'>Home</NavLink>
+          <NavLink to='/dashboard' className='nav-link'>Dashboard</NavLink>
+          <NavLink to='/login' className='nav-link'>Login</NavLink>
+        </Nav>
+        <Routes>
+          <Route path='/' element={<Home/>}/>
+          <Route path='/dashboard' element={<Dashboard/>}/>
+          <Route path='/login' element={<Login/>}/>
+        </Routes>
+        <Footer>
+          Test Complex, 2021
+        </Footer>
+      </div>
+    </Router>
+  );
+}
+
+export default App;
