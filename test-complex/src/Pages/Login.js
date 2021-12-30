e4e36fa3395ddd1dd6acded88f8b0e21e20ce3e6
e.demidova <kat.demidoffa@gmail.com>
2021-12-30 19:02:47 +0300

    feat: edit logic and add dynamics to the button

--- test-complex/src/Pages/Login.js
+++ test-complex/src/Pages/Login.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styled from 'styled-components';
 
 const Wrap = styled.div`
@@ -44,23 +44,65 @@ const ButtonField = styled.div`
 `;
 
 const Button = styled.button`
-  padding: 10px 40px;
+  width: 140px;
+  height: 40px;
   background-color: #8DD2E8;
   border: 1px solid black;
 `;
 
-const handleSubmit = () => {
-  console.log('Hello');
-  fetch("https://ekaterinademidova.us.auth0.com/oauth/token?grant_type=password&username=test@gmail.com&password=testuser&audience=http://test-complex/api&scope=openid&client_id=3VC6lHo7FgInGl68nbtqtVNhhFyOteaS&client_secret=JKRueYrPOyyTn-aRoNyCGdHEW-7cJ-kVQ3mPtQTLkAdN7YMN16Qte-uQbEYocz80", {
-    method: "POST",
-    headers: {'Content-Type': 'application/json'}
-    // body: JSON.stringify(data)
-  }).then(res => {
-    console.log("Request complete! response:", res);
-  });
-};
+const Error = styled.p`
+  width: 100%;
+  height: 16px;
+  font-size: 14px;
+  text-align: center;
+  color: red;
+`;
 
 export default function Login() {
+
+  let [buttonValue, setButtonValue] = useState('Войти');
+  let [buttonDisabled, setButtonDisabled] = useState(false);
+  let [statusRequest, setStatusRequest] = useState(null);
+  let [showError, setShowError] = useState(false);
+
+  const handleSubmit = () => {
+
+    setButtonValue('Ожидайте...');
+    setButtonDisabled(true);
+    
+    var data = {
+      grant_type: 'password',
+      username: 'test@gmail.com',
+      password: 'testuser',
+      audience: 'http://test-complex/api',
+      scope: 'openid',
+      client_id: '3VC6lHo7FgInGl68nbtqtVNhhFyOteaS',
+      client_secret:"JKRueYrPOyyTn-aRoNyCGdHEW-7cJ-kVQ3mPtQTLkAdN7YMN16Qte-uQbEYocz80"
+    };
+    fetch("https://ekaterinademidova.us.auth0.com/oauth/token", {
+      credentials: 'include',
+      mode: 'cors',
+      method: "POST",
+      headers: {
+        'Content-Type': 'application/x-www-form-urlencoded'},
+      body: JSON.stringify(data)
+    }).then(res => {
+      console.log("Request complete! response:", res);
+    });
+
+    setTimeout(() => {
+      if (statusRequest) {
+        setShowError(false);
+        window.location.assign('http://localhost:3000/dashboard/');
+      } else {
+        setShowError(true);
+      }
+
+      setButtonValue('Войти');
+      setButtonDisabled(false);
+    }, 2000);
+  };
+
   return (
     <div>
       <Wrap>
@@ -78,8 +120,12 @@ export default function Login() {
           </Field>
 
           <ButtonField>
-            <Button onClick={handleSubmit}>Войти</Button>
+            <Button onClick={handleSubmit} disabled={buttonDisabled}>{buttonValue}</Button>
           </ButtonField>
+          { showError 
+            ? <Error>Неверно введенны данные.</Error>
+            : null
+          }
         </LoginWrap>
       </Wrap>
     </div>
